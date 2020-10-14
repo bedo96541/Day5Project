75f453e7f18ca9238cffe47e4793a0bc94797f64
BedrettinErkan <bedrettinerkan123@gmail.com>
2020-10-14 16:28:23 +0300

    changing line

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("This is myn own hello");
         //add one more line on your local and commit
     }
 }
