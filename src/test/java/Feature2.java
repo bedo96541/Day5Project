b08318c418f1e95c84bb190d10ddb5cffcc3a7a6
BedrettinErkan <bedrettinerkan123@gmail.com>
2020-10-14 16:06:24 +0300

    awasome feature 2 commit more

--- src/test/java/Feature2.java
+++ src/test/java/Feature2.java
@@ -3,5 +3,7 @@ public class Feature2 {
         System.out.println("Working on feature2");
         System.out.println("Working on feature2 more");
         System.out.println("Working on feature2 and more and more");
+
+        System.out.println("Adding more line as you request ");
     }
 }
